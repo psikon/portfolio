6d3828f0c7b2fc058af542d020b394fa664e7044
Philipp Hennersdorf <Philipp.Hennersdorf@encoway.de>
2020-01-07 14:20:53 +0100

    adjust layout

--- src/components/About/AboutImage.js
+++ src/components/About/AboutImage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const aboutImage = (props) => {
-    return <img className="picture" width={props.width} src={props.picture} alt={props.caption} />
+    return <img className="picture" src={props.picture} alt={props.caption} />
 }
 
 export default aboutImage;
